3fc999edd9fcd746faf45c201beae492039fb781
Jiseong <112401739+Jisong2@users.noreply.github.com>
2026-09-14 10:31:38 +0900

    [Passed] 몫과 나머지 (1ms, 0MB)

---
+++ trail1/몫과 나머지/quotient-remainder.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+
+    int a ,b,c,d;
+   
+
+    scanf("%d %d",&a,&b);
+
+    c = a/b;
+    d = a%b;
+    printf("%d...%d",c,d);
+    return 0;
+}
